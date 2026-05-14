dc3012419529149a7e253bbd24ecb37a24927d04
anisiiayalyzhko <anisiya.sumy.ua@gmail.com>
2026-05-14 18:56:53 +0300

    Add Catalogue class

---
+++ src/main/java/org/example/Catalogue.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import org.example.exception.BookNotFoundException;
+import org.example.model.Publication;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Catalogue {
+    private List<Publication> publications;
+
+    public Catalogue() {
+        this.publications = new ArrayList<>();
+    }
+
+    public void addPublication(Publication p) {
+        publications.add(p);
+    }
+
+    public void removePublicationByTitle(String title) throws BookNotFoundException {
+        Publication toRemove = findPublicationByTitle(title);
+        if (toRemove == null) {
+            throw new BookNotFoundException("Publication with title '" + title + "' was not found.");
+        }
+        publications.remove(toRemove);
+    }
+
+    public Publication findPublicationByTitle(String title) {
+        for (Publication p : publications) {
+            if (p.getTitle().toLowerCase().contains(title.toLowerCase())) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public List<Publication> getAllPublications() {
+        return new ArrayList<>(publications);
+    }
+
+    public void saveToFile(String filename) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(publications);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void loadFromFile(String filename) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            this.publications = (List<Publication>) ois.readObject();
+        }
+    }
+}
